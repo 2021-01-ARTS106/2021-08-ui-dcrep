90b85de3cc21a00c66770d01805e30b36111ef5a
Daniel C <63077752+dcrep@users.noreply.github.com>
2021-03-29 23:21:35 -0400

    Tutorial3Done

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -8,6 +8,9 @@ public class UIManager : MonoBehaviour
 	public Animator startButton;
 	public Animator settingsButton;
 	public Animator dialog;
+	public Animator contentPanel;
+	public Animator gearImage;
+	
 	public void OpenSettings()
 	{
 		startButton.SetBool("isHidden", true);
@@ -19,6 +22,12 @@ public class UIManager : MonoBehaviour
 		startButton.SetBool("isHidden", false);
 		settingsButton.SetBool("isHidden", false);
 		dialog.SetBool("isHidden", true);
+	}
+	public void ToggleMenu() 
+	{
+		bool isHidden = contentPanel.GetBool("isHidden");
+		contentPanel.SetBool("isHidden", !isHidden);
+		gearImage.SetBool("isHidden", !isHidden);
 	}
 	public void StartGame() 
 	{
